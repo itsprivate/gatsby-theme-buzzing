7220bea6c3854af66a0932d0428f60958b451c28
Owen Young <theowenyoung@gmail.com>
2020-11-25 20:44:58 +0800

    feat: add add to any

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+exports.onInitialClientRender = _ => {
+  var s = document.createElement("script")
+  s.type = "text/javascript"
+  s.innerText =
+    'var a2a_config=a2a_config||{};function my_addtoany_onready(){for(var a=document.getElementsByClassName("share-button"),n=0;n<a.length;n++)a[n].classList.add("a2a_dd");a2a.init("page",{target:".share-this"})}function initShare(){a2a_config.callbacks=a2a_config.callbacks||[],a2a_config.callbacks.push({ready:my_addtoany_onready})}navigator.share||initShare();'
+  document.body.appendChild(s)
+  const addThisScript = document.createElement("script")
+  addThisScript.src = `//static.addtoany.com/menu/page.js`
+  addThisScript.async = true
+  document.body.appendChild(addThisScript)
+}
--- src/gatsby-theme-timeline/components/reddit/item-footer.js
+++ src/gatsby-theme-timeline/components/reddit/item-footer.js
@@ -3,12 +3,17 @@ import { jsx, Flex } from "theme-ui"
 import ItemDate from "gatsby-theme-timeline/src/components/reddit/item-date"
 import ItemSource from "./item-source"
 import ShareIcon from "./share-icon"
+import i18next from "i18next"
+
 export default function ({ item }) {
+  const { dateISO } = item
+  const year = new Date(dateISO).getUTCFullYear()
+  const title = i18next.t(`reddit-title-${year}__::::__${item.title}`)
   const handleShare = () => {
     if (navigator.share) {
       navigator
         .share({
-          title: item.title,
+          title: title,
           url: `https://www.reddit.com${item.permalink}`,
         })
         .then(() => {
@@ -38,8 +43,18 @@ export default function ({ item }) {
         <ItemSource item={item}></ItemSource>
       </Flex>
       <Flex>
-        <div role="button" onClick={handleShare} sx={{ cursor: "pointer" }}>
-          <ShareIcon></ShareIcon>
+        <div
+          className="a2a_kit share-this"
+          data-a2a-title={title}
+          data-a2a-url={`https://www.reddit.com${item.permalink}`}
+          role="button"
+          tabIndex={0}
+          onClick={handleShare}
+          sx={{ cursor: "pointer" }}
+        >
+          <a className="share-button" href="https://www.addtoany.com/share">
+            <ShareIcon></ShareIcon>
+          </a>
         </div>
       </Flex>
     </footer>
