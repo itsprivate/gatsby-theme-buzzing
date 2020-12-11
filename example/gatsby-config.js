240e84694be71561714c505324f97035977e8dae
Owen Young <theowenyoung@gmail.com>
2020-12-11 14:44:11 +0800

    feat: better for example

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -45,11 +45,43 @@ plugins = plugins.concat([
       },
     },
   },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Reddit热门`,
+      short_name: `Reddit热门`,
+      start_url: `/`,
+      lang: `zh`,
+      description: `用中文浏览Reddit上的热门内容`,
+      background_color: `#f7f0eb`,
+      theme_color: `#FF4500`,
+      display: `standalone`,
+      icon: `src/images/icon.png`,
+      localize: [
+        {
+          start_url: `/en/`,
+          lang: `en`,
+          name: `Reddit Top`,
+          short_name: `Reddit`,
+          description: `The most popular posts on Reddit`,
+        },
+      ],
+    },
+  },
+  {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-GQVR44BC8T", // Google Analytics / GA
+      ],
+    },
+  },
 ])
 module.exports = {
   plugins: plugins,
   siteMetadata: {
-    title: `Buzzing on Example`,
+    title: `Buzzing on Reddit`,
     author: `Reddit`,
     description: `See what's buzzing on Reddit in your native language`,
     siteUrl: "https://reddit.owenyoung.com",
---
+++ gatsby-theme-buzzing/README.md
@@ -0,0 +1,5 @@
+# Gatsby Theme Buzzing
+
+For buzzing.
+
+Todo
