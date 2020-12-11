cb2b80fc8e9e8eecc75bf052e8a93cddf75eafe3
Owen Young <theowenyoung@gmail.com>
2020-12-11 11:21:20 +0800

    feat: add footer

---
+++ src/gatsby-theme-timeline/components/home-footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { css } from "theme-ui"
+
+const Footer = () => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+    © {new Date().getFullYear()}
+  </footer>
+)
+export default Footer
