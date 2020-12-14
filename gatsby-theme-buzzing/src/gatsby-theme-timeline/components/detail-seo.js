cd3edbb8a907d7cf03d177ba95702ec05207c212
Owen Young <theowenyoung@gmail.com>
2020-12-14 19:43:54 +0800

    fix: detail seo title

--- gatsby-theme-buzzing/src/gatsby-theme-timeline/components/detail-seo.js
+++ gatsby-theme-buzzing/src/gatsby-theme-timeline/components/detail-seo.js
@@ -1,27 +1,29 @@
 import React from "react"
 import SEO from "gatsby-theme-timeline/src/components/seo"
+import { kebabToSnakeCase } from "../../util"
 
 export default ({ item, location, pageContext: { locale } }) => {
   let description = item.excerpt
   let title = item.title
+  let finalLocale = kebabToSnakeCase(locale)
 
   if (item.__typename === "RedditPost") {
     if (
       item.parent &&
       item.parent.i18nResource &&
-      item.parent.i18nResource[locale] &&
-      item.parent.i18nResource[locale].title
+      item.parent.i18nResource[finalLocale] &&
+      item.parent.i18nResource[finalLocale].title
     ) {
-      title = item.parent.i18nResource[locale].title
+      title = item.parent.i18nResource[finalLocale].title
     }
     if (item.parent && item.parent.the_new_excerpt) {
       description = item.parent.the_new_excerpt
       if (
         item.parent.i18nResource &&
-        item.parent.i18nResource[locale] &&
-        item.parent.i18nResource[locale].the_new_excerpt
+        item.parent.i18nResource[finalLocale] &&
+        item.parent.i18nResource[finalLocale].the_new_excerpt
       ) {
-        description = item.parent.i18nResource[locale].the_new_excerpt
+        description = item.parent.i18nResource[finalLocale].the_new_excerpt
       }
     }
   }
